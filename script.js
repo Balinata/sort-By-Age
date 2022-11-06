197d4a9e87f9d024f94e6557bd806187f6b5ae18
natali <devidvarl@gmail.com>
2022-11-06 06:27:14 +0100

    function

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let User1 = { name: "User1", age: 25 };
 let User2 = { name: "User2", age: 30 };
 let User3 = { name: "User3", age: 28 };
 let arr = [ User1, User2, User3];
+function sortByAge(arr) {
+    arr.sort((a, b) => a.age > b.age ? 1 : -1);
+  }
